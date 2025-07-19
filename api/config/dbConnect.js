9f75adeb3a5af596cb1b507cc00693ee5fd780d1
ajuuz <ajuuz07@gmail.com>
2025-07-20 00:54:14 +0530

    check1

--- api/config/dbConnect.js
+++ api/config/dbConnect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 function dbConnect(){
+    console.log(process.env.DATABASE_URI)
     mongoose.connect(process.env.DATABASE_URI)
     .then(()=>console.log('database connected'))
     .catch(()=>console.log('failed to connect database'))
